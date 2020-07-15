fd6e578b72dd416422b3029c43c652706d7430c0
Afonso Machado <afonso777@hotmail.com>
2020-07-15 00:27:36 -0300

    implementação da navbar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,23 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
-// Importando modulo do angular material
-import { MatToolbarModule } from "@angular/material/toolbar";
+// Importando modulos criados
+import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+// Importando modulos do angular material
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from "@angular/material/sidenav";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent] //Inicialização do componente
