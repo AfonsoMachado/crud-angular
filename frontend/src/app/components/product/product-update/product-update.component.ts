1167e5a75c9ab5f971b23fa863646b5dc6861504
Afonso Machado <afonso777@hotmail.com>
2020-07-16 18:47:34 -0300

    implemntado product-update

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,3 +1,6 @@
+import { Product } from './../product.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product: Product
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // Capturando o parametro que tem como chave o id (definido nas rotas), rtecebido pela rota
+    const id = this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso')
+      //Sai da tela e volta pra tela de produto
+      this.router.navigate(["/products"])
+    })
+  }
+
+  /**
+   * Cancela a ação, retornando para a tela com a tabela dos produtos
+   */
+  cancel(): void {
+    // Retorna para a tela com a tabela dos produtos
+    this.router.navigate(['/products'])
   }
 
 }
